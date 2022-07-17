56f60d8209f40e3dbdb065f0978f6b4d3a4591de
Tristan Schrader <schrader.tristan@gmail.com>
2022-07-17 01:59:39 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+	description = "A flake for building Hello World";
+	inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+	outputs = { self, nixpkgs }: {
+		defaultPackage.x86_64-linux =
+			with import nixpkgs { system = "x86_64-linux"; };
+			stdenv.mkDerivation {
+				name = "hello";
+				src = self;
+				buildPhase = "gcc -o hello ./hello.c";
+				installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+			};
+	};
+}
